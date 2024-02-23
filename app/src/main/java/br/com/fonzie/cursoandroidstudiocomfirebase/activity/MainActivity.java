f0a86359273abd15a21ee8b9977bed2518426cfa
arturbazza <agencia@fonzie.com.br>
2024-02-23 16:18:48 -0300

    Design do projeto Flowers

---
+++ app/src/main/java/br/com/fonzie/cursoandroidstudiocomfirebase/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.fonzie.cursoandroidstudiocomfirebase.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.fonzie.cursoandroidstudiocomfirebase.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
